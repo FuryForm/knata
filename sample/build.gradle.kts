b1610be49f773b47859313ea04b8b77ccd10080c
Varavut Lormongkol <varavut@users.noreply.github.com>
2026-04-20 18:42:43 +0700

    add kts

---
+++ sample/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.furyform.knata.sample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.furyform.knata.sample"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(project(":knata"))
+    implementation(libs.kotlinx.serialization.json)
+
+    val composeBom = platform(libs.compose.bom)
+    implementation(composeBom)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material.icons)
+    implementation(libs.activity.compose)
+
+    debugImplementation(libs.compose.ui.tooling)
+}
+
